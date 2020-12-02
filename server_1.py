afe364e4931ce0f16fa7bd441ada2adf3bc2707f
zimaCOOL <zimacl@zimaCL-PC.zima>
2020-12-02 03:56:05 +0200

    chenched server.py_1.py

--- server_1.py
+++ server_1.py
@@ -1,6 +1,13 @@
 from flask import Flask
-app = Flask(__name__)
+app=Flask(__name__)
 
 @app.route('/')
+def index_page():
+    return '<h1>Index page</h1>'
+
+
+@app.route('/hello')
 def hello_world():
-    return 'Hello, World!'
+    return '<h2>Hello, World!</h2>'
+
+
