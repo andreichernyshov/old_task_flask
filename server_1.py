34d8e652796017e7e3bd87ae2a518ba3fd5c83ba
zimaCOOL <zimacl@zimaCL-PC.zima>
2020-12-04 18:18:16 +0200

    add function to fill fields in generate_users.html

--- server_1.py
+++ server_1.py
@@ -4,11 +4,6 @@ from markupsafe import escape
 
 server_1=Flask(__name__)
 
-req = list()
-with open('requirements.txt') as packejes:
-    for line in packejes:
-        line = f"{line} "
-        req.append(line)
 
 
 @server_1.route('/')
@@ -20,6 +15,25 @@ def index():
 def requirements():
     return render_template('requirements.html', title=requirements, req=req)
 
-     
+
+req = list()
+with open('requirements.txt') as packejes:
+    for i in packejes:
+        i = f"{i} "
+        req.append(i)
+
+
+@server_1.route('/generate_users/')
+def generate_users():
+    return render_template('generate_users.html', title=generate_users, us_ml=us_ml)
+
+
+us_ml = list()
+with open('fake_list.txt') as usr_mail:
+    for x in usr_mail:
+        x = f"{x} "
+        us_ml.append(x)
+
+
 if __name__ == "__main__":
     server_1.run(debug=True)
