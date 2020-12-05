65f7aee76e3395c229014ec2a50b1852bf432b06
zimaCOOL <zimacl@zimaCL-PC.zima>
2020-12-05 22:10:01 +0200

    realize function for parsing data from .csv tab as from matrix, count values, return results as content to mean.html

--- server_1.py
+++ server_1.py
@@ -1,6 +1,7 @@
 from flask import Flask, url_for, render_template
-import os, glob
+import os, glob, csv
 from markupsafe import escape
+from pathlib import Path
 
 server_1=Flask(__name__)
 
@@ -33,6 +34,35 @@ with open('fake_list.txt') as usr_mail:
     for x in usr_mail:
         x = f"{x} "
         us_ml.append(x)
+
+
+"""
+p = Path("/home/zimacl/Python_")
+f = p.joinpath("Python_/hw.csv")
+"""
+
+
+@server_1.route('/mean/')
+def mean():
+    ls_d = list()
+    with open('hw.csv', encoding='utf-8') as data_file:
+        for line in data_file:
+            if len(line) <= 2:
+                continue
+            else:
+                line = list(line.strip().split(','))
+                ls_d.append(line)
+        height, weight = 0, 0
+        for row in range(1, len(ls_d)):
+            for col in range(1, len(ls_d[0])):
+                if col == 1:
+                    height += float(ls_d[row][col])
+                if col == 2:
+                    weight += float(ls_d[row][col])
+    santim_avarg = round(((height/(len(ls_d)-1)) * 2.54), 0)
+    kg_avarg = round(((weight/(len(ls_d)-1))*0.45), 0)
+    conten = f'Рост в см. средний = {santim_avarg}.  Вес в кг. средний = {kg_avarg}'
+    return render_template('mean.html', title=mean, conten=conten)
 
 
 if __name__ == "__main__":
